513e042a17033bd93c33552c82ebe712eaf3c794
RezaSh798 <rezakhorramisalarabadi@gmail.com>
2025-08-17 22:07:39 +0330

    Feature: Create user entity and roles enum

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string UserName { get; set; }
+    public Roles Role { get; set; } = Roles.Member;
+}
